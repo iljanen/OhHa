6497cf5506d50263c02d52d28d43359aa699cbb1
iljanen <ilja.vanhanen@gmail.com>
2013-08-01 15:56:57 +0300

    muutoksia

--- OhHa-tyo/src/ohha/tyo/OhHaTyo.java
+++ OhHa-tyo/src/ohha/tyo/OhHaTyo.java
@@ -14,6 +14,8 @@ public class OhHaTyo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Henkilo kaveri = new Kaveri("Mika", 23, "0404404440");
+        System.out.println(kaveri.tulosta());
     }
 }
